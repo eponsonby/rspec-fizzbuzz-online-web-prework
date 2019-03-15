67d286f2506b46ac37c0cbbc1b859e58a25a3833
Erin Ponsonby <erinponsonby@gmail.com>
2019-03-15 15:48:27 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,7 +5,7 @@ def fizzbuzz(num)
     return "Fizz"
   elsif num % 5 == 0 && num % 3 !== 0
     return "Buzz"
-  elsif num % 3 == 0 && num % 5 == 0
+  else num % 3 == 0 && num % 5 == 0
     return "FizzBuzz"
   end
 end
